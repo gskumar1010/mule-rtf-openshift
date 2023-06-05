91aa84f54d787514c3836bdd883fb548a11381f4
Tosin Akinosho <takinosh@redhat.com>
2023-06-04 22:16:09 -0400

    updating code

---
+++ README.md
@@ -0,0 +1,47 @@
+# Creating Runtime Fabric RTF on OpenShift
+
+This guide provides instructions for setting up a Runtime Fabric (RTF) on OpenShift. It includes both automated steps using provided scripts and manual steps to be followed. RTF is a deployment option for Mule runtimes that allows you to run Mule applications in a self-managed infrastructure.
+
+## Automated Steps 
+`TO-DO automate AMQ Deployment`
+Before proceeding with the automated steps, make sure you meet the following prerequisites:
+- An active OpenShift cluster
+- OpenShift CLI installed and configured
+- Cluster-admin role assigned to the logged-in user
+- Runtime Fabric Operator installed in the OpenShift cluster
+- `license_rh_org.lic` file downloaded from https://anypoint.mulesoft.com/ and available locally
+- `git` installed on the local machine
+
+### Quickstart Script
+To quickly set up the Runtime Fabric on OpenShift, follow these steps:
+1. Download the quickstart script:
+   ```
+   curl -OL https://raw.githubusercontent.com/tosin2013/mule-rtf-openshift/main/installing-rtf-openshift/quickstart.sh
+   chmod +x quickstart.sh
+   ```
+
+2. Run the script:
+   ```
+   ./quickstart.sh
+   ```
+
+### Clean up Script
+To clean up the Runtime Fabric deployment, follow these steps:
+1. Download the cleanup script:
+   ```
+   curl -OL https://raw.githubusercontent.com/tosin2013/mule-rtf-openshift/main/installing-rtf-openshift/cleanup-env.sh
+   chmod +x cleanup-env.sh
+   ```
+
+2. Run the script:
+   ```
+   ./cleanup-env.sh
+   ```
+
+## Manual Steps
+Follow the manual steps listed below to create and configure the Runtime Fabric on OpenShift.
+
+[Creating Runtime Fabric RTF on OpenShift](installing-rtf-openshift/README.md)
+
+## Troubleshooting RTF Installation
+If you encounter the error message "Couldn't validate license key!", make sure that the license value in the RuntimeFabric Instance YAML file matches the base64-encoded value of the MuleSoft license.
--- installing-rtf-openshift/quickstart.sh
+++ installing-rtf-openshift/quickstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 # This script is used to install the Runtime Fabric Operator and Runtime Fabric on OpenShift.
 
-#scp ~/Downloads/license_rh_org.lic   azure@bastion.4psh8.azure.opentlc.com:/tmp  
+#scp ~/Downloads/license_rh_org.lic   admin@sample.jumpbox.com:/tmp  
 
 function dependency_check() {
     if ! yq -v  &> /dev/null
@@ -31,19 +31,20 @@ fi
 dependency_check
 
 read -p "Enter the value provided on the RTF page for 'Project Name': " projectname
-read -p "Enter the value provided on the RTF page for 'docker-password': " dockerPassword
+read -p "Enter the value provided on the RTF page for 'docker-username': " dockerUsername
 read -p "Enter the value provided on the RTF page for 'docker-password': " dockerPassword
 read -p "Enter the value provided on the RTF page for 'Activation Data': " activationdata
 oc new-project $projectname
 oc create secret docker-registry rtf-pull-secret --namespace rtf --docker-server=rtf-runtime-registry.kprod.msap.io --docker-username="$dockerUsername" --docker-password="$dockerPassword"
 
- oc create  sa sa-with-anyuid
- oc adm policy add-scc-to-user anyuid -z sa-with-anyuid
+oc create  sa sa-with-anyuid
+oc adm policy add-scc-to-user anyuid -z sa-with-anyuid
 
- base64 -i /tmp/license_rh_org.lic > output.txt
+base64 -i /tmp/license_rh_org.lic > $HOME/output.txt
 
-yq e -i  '.metadata.namespace = "'$projectname'"' installing-rtf-openshift/configs/runtimefabric.yaml
-yq e -i  '.spec.activationData = "'$activationdata'"' installing-rtf-openshift/configs/runtimefabric.yaml
-yq e -i  '.spec.muleLicense = "'$(cat output.txt)'"'  installing-rtf-openshift/configs/runtimefabric.yaml
-oc apply -f installing-rtf-openshift/configs/runtimefabric.yaml
-echo "installing-rtf-openshift/configs/runtimefabric.yaml" > .gitignore
+yq e -i  '.metadata.namespace = "'$projectname'"' installing-rtf-openshift/configs/runtimefabric.yaml > $HOME/runtimefabric.yaml
+yq e -i  '.spec.activationData = "'$activationdata'"' $HOME/runtimefabric.yaml
+item="$(< $HOME/output.txt)" yq e -i '.spec.muleLicense = env(item)' $HOME/runtimefabric.yaml
+cat $HOME/runtimefabric.yaml
+sleep 5s
+oc apply -f $HOME/runtimefabric.yaml
